f6cfeeccb281b558012adb5a850f5e9fb8a5dc5e
bartbart333 <bartholdbos@hotmail.com>
2014-02-17 13:19:58 +0100

    Socket now has a DatagramSocket

--- src/tk/bartbart333/udp/Socket.java
+++ src/tk/bartbart333/udp/Socket.java
@@ -1,8 +1,15 @@
 package tk.bartbart333.udp;
 
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 public class Socket extends Thread{
 	
-	public Socket(){
+	private DatagramSocket datasocket;
+	
+	public Socket() throws SocketException{
+		datasocket = new DatagramSocket();
+		
 		this.start();
 	}
 	
