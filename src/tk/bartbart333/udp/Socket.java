0a458dc99342b2e5beaf99768df8594cee249e41
bartbart333 <bartholdbos@hotmail.com>
2014-02-20 00:29:05 +0100

    Socket can now read Packets

--- src/tk/bartbart333/udp/Socket.java
+++ src/tk/bartbart333/udp/Socket.java
@@ -1,14 +1,17 @@
 package tk.bartbart333.udp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class Socket extends Thread{
 	
 	private DatagramSocket datasocket;
 	private boolean running = true;
+	private ArrayList<Connection> connections = new ArrayList<Connection>();
 	
 	public Socket() throws SocketException{
 		datasocket = new DatagramSocket();
@@ -31,9 +34,22 @@ public class Socket extends Thread{
 	public void run(){
 		byte[] data = new byte[1024 * 16];
 		DatagramPacket datapacket = new DatagramPacket(data, data.length);
+		Packet packet;
 		
 		while(running){
-			
+			try{
+				datasocket.receive(datapacket);
+				
+				for(Connection connection : connections){
+					if(datapacket.getAddress().equals(connection.getAddress()) && datapacket.getPort() == connection.getPort()){
+						packet = new Packet(datapacket.getData(), datapacket.getLength());
+						
+						connection.addPacket(packet);
+					}
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 		}
 		
 		return;
